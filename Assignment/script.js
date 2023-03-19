a7e57d7575de77955d789fe12ed903534aa5a943
delazzer27 <tomas.delazzer@gmail.com>
2023-03-18 21:18:43 -0300

    fixing structure

--- Assignment/script.js
+++ Assignment/script.js
@@ -7,9 +7,10 @@ let addNumber = 0;
 let numberDisplay = "";
 let adding = false;
 let subtracting = false;
-let firsOperate = true;
 let multiplying = false;
 let dividing = false;
+let flagFirstOperation = true;
+let flagOperator = false;
 
 
 //FUNCTIONS
@@ -29,28 +30,31 @@ function divide (num1, num2){
     return num1/num2;
 }
 
-// function operate (operator, num1, num2){
-//     switch (operator) {
-//         case add:
-//         return add(num1, num2);
-//         case subtract:
-//         return subtract(num1, num2);
-//         case multiply:
-//         return multiply(num1, num2);
-//         case divide:
-//         return divide(num1, num2)
-//     }
-// }
-
 function clearOperation (){
-    input.value = 0;
+    addNumber = 0;
+    numberDisplay = "";
+    flagFirstOperation = true;
+    flagOperator = false;
+    input.value = addNumber;
 }
 
 //Show display
 btn.forEach(element => {
     element.addEventListener("click", ()=> {
-        input.value = numberDisplay + element.value;
-        numberDisplay = input.value;
+        if(!flagOperator){
+            input.value = numberDisplay + element.value;
+            numberDisplay = input.value;
+        }
+        else{
+            numberDisplay = "";
+            input.value = numberDisplay + element.value;
+            numberDisplay = input.value;
+            flagFirstOperation = false;
+        }
+        
+
+        flagOperator = false;
+        // calculate();
     })
 });
 
@@ -60,120 +64,66 @@ clear.addEventListener("click", ()=>{
 
 operator.forEach(element => {
     element.addEventListener("click", ()=>{
+        flagOperator = true;
+        if(!flagFirstOperation){
+            calculate();
+            flagFirstOperation=true;
+        }
         switch (element.value) {
             case "+":
-                if (subtracting){
-                    addNumber = subtract(addNumber, parseInt(numberDisplay));
-                    input.value = addNumber;
-                } else if (multiplying) {
-                    addNumber = multiply(addNumber, parseInt(numberDisplay));
-                    input.value = addNumber;
-                } else if (dividing){
-                    addNumber = divide(addNumber, parseInt(numberDisplay));
-                    input.value = addNumber;
-                } else {
-                    addNumber = add(addNumber, parseInt(numberDisplay));
-                    input.value = addNumber;
-                }
                 adding = true;
                 subtracting = false;
-                firsOperate = false;
                 multiplying = false;
                 dividing = false;
                 break;
             case "-":
-                if (firsOperate == false){
-                    if (adding){
-                        addNumber = add(addNumber, parseInt(numberDisplay));
-                        input.value = addNumber;
-                    } else if (multiplying){
-                        addNumber = multiply(addNumber, parseInt(numberDisplay));
-                        input.value = addNumber;
-                    } else if (dividing){
-                        addNumber = divide(addNumber, parseInt(numberDisplay));
-                        input.value = addNumber;
-                    } else {
-                        addNumber = subtract(addNumber, parseInt(numberDisplay));
-                        input.value = addNumber;
-                    }
-                } else {
-                    addNumber = parseInt(numberDisplay);
-                    firsOperate = false;
-                }
                 adding = false;
                 subtracting = true;
-                multiplying = false
+                multiplying = false;
                 dividing = false;
                 break;
             case "*":
-                if (firsOperate == false){
-                    if (adding){
-                        addNumber = add(addNumber, parseInt(numberDisplay));
-                        input.value = addNumber;
-                    } else if (subtracting){
-                        addNumber = subtract(addNumber, parseInt(numberDisplay));
-                        input.value = addNumber;
-                    } else if (dividing){
-                        addNumber = divide(addNumber, parseInt(numberDisplay));
-                        input.value = addNumber;
-                    } else {
-                        addNumber = multiply(addNumber, parseInt(numberDisplay));
-                        input.value = addNumber;
-                    }
-                } else {
-                    addNumber = parseInt(numberDisplay);
-                    firsOperate = false;
-                }
                 adding = false;
                 subtracting = false;
                 multiplying = true;
                 dividing = false;
                 break;
-                case "/":
-                    if (firsOperate == false){
-                        if (adding){
-                            addNumber = add(addNumber, parseInt(numberDisplay));
-                            input.value = addNumber;
-                        } else if (subtracting){
-                            addNumber = subtract(addNumber, parseInt(numberDisplay));
-                            input.value = addNumber;
-                        } else if (multiplying){
-                            addNumber = multiply(addNumber, parseInt(numberDisplay));
-                            input.value = addNumber;
-                        } else {
-                            addNumber = divide(addNumber, parseInt(numberDisplay));
-                            input.value = addNumber;
-                        }
-                    } else {
-                        addNumber = parseInt(numberDisplay);
-                        firsOperate = false;
-                    }
-                    adding = false;
-                    subtracting = false;
-                    multiplying = false;
-                    dividing = true;
+            case "/":
+                adding = false;
+                subtracting = false;
+                multiplying = false;
+                dividing = true;
         }
-        numberDisplay = "";
+        addNumber = input.value;
+
     })
 })
 
 result.addEventListener("click", ()=>{
-    if (adding) {
-        input.value = add(addNumber, parseInt(numberDisplay));
-    } else if (subtracting) {
-        input.value = subtract(addNumber, parseInt(numberDisplay));
-    } else if (multiplying) {
-        input.value = multiply(addNumber, parseInt(numberDisplay));
-    } else if (dividing){
-        input.value = divide(addNumber, parseInt(numberDisplay));
-    }
+    calculate();
 })
-    input.value = 0;
 
 
+// function booleanValues (booleanAdd, booleanSub, booleanFO, booleanMul, booleanDiv) {
+//     adding = booleanAdd;
+//     subtracting = booleanSub;
+//     firsOperate = booleanFO;
+//     multiplying = booleanMul;
+//     dividing = booleanDiv;
+// }
 
+function calculate (){
+    if (adding){
+        addNumber = add(parseInt(addNumber), parseInt(numberDisplay));
+    } else if (multiplying){
+        addNumber = multiply(parseInt(addNumber), parseInt(numberDisplay));
+    } else if (dividing){
+        addNumber = divide(parseInt(addNumber), parseInt(numberDisplay));
+    } else if(subtracting) {
+        addNumber = subtract(parseInt(addNumber), parseInt(numberDisplay));
+    }
+    input.value = addNumber;
+}
 
-
-
-
+// input.value = 0;
 
